9c20d198b8c52df94b3f0f9808f264b88a1c1f02
Veshal Maniam <69617912+veshalManiam21@users.noreply.github.com>
2022-03-17 06:34:01 +0800

    Login Button
    Added several reusable components. and Installed type-fest

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,19 @@ import React from "react";
 import type { GetStaticProps } from "next";
 import { NextPage } from "../typings/types";
 import { InputText } from "@/components/InputText/InputText";
+import { Button } from "@/components/Button/Button";
 
 type IndexPageProps = {};
 
 const IndexPage: NextPage<IndexPageProps> = () => {
+  const login = () => {};
+
   return (
     <main className="min-h-screen">
       <div className="container absolute top-0 left-0 right-0 bottom-0 m-auto h-min">
         <div className="border border-white bg-black-0c0f13 rounded-lg p-8 max-w-xs m-auto">
           <h3 className="text-2xl mb-4 text-center">Login Page</h3>
-          <div className="w-full space-y-2">
+          <div className="w-full space-y-2 mb-4">
             <InputText placeholder="Email" autoComplete="no" />
             <InputText
               type="password"
@@ -20,6 +23,13 @@ const IndexPage: NextPage<IndexPageProps> = () => {
               autoComplete="no"
             />
           </div>
+          <Button
+            className="w-full"
+            customColorClassName="text-black-0c0f13 bg-white hover:bg-black-0c0f13 hover:text-white"
+            onClick={login}
+          >
+            Login
+          </Button>
         </div>
       </div>
     </main>
