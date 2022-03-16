6f2656efcd0fbef3efc8375bd646d17064b992a6
Veshal Maniam <69617912+veshalManiam21@users.noreply.github.com>
2022-03-17 05:51:56 +0800

    Login Page
    No Api Integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
-import type { NextPage } from "next";
+import React from "react";
 
-const Home: NextPage = () => {
-  return <div>Hello world</div>;
+import type { GetStaticProps } from "next";
+import { NextPage } from "../typings/types";
+import { InputText } from "@/components/InputText/InputText";
+
+type IndexPageProps = {};
+
+const IndexPage: NextPage<IndexPageProps> = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="container absolute top-0 left-0 right-0 bottom-0 m-auto h-min">
+        <div className="border border-white rounded-lg p-4 max-w-md m-auto">
+          <h3 className="text-2xl mb-4 text-center">Login Page</h3>
+          <div className="w-full space-y-2">
+            <InputText placeholder="Email" autoComplete="no" />
+            <InputText
+              type="password"
+              placeholder="Password"
+              autoComplete="no"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Home;
+export const getStaticProps: GetStaticProps<IndexPageProps> = async () => {
+  return {
+    props: {},
+  };
+};
+
+export default IndexPage;
